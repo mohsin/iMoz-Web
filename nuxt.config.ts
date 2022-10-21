ed7ca4e96a40c137b986c0b21fdd4fa3115dc9ae
Saifur Rahman Mohsin <mohsin92@me.com>
2022-10-22 00:10:19 +0530

    feat: Added tailwindcss, basic layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    css: ["@/assets/css/styles.css"],
     nitro: {
         preset: 'netlify-edge'
-    }
+    },
+    build: {
+      postcss: {
+        postcssOptions: require("./postcss.config.js"),
+      },
+    },
 })
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="border border-black">
+        <h1>Welcome to the homepage</h1>
+    </div>
+</template>
