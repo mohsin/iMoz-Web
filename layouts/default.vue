5626782ae81c69e35da4d2c8b33c8c601d25ce7e
Saifur Rahman Mohsin <mohsin92@me.com>
2022-10-23 02:24:35 +0530

    feat: Navbar with dark mode switch
    * Added navbar with icons for dark and light modes.
    * Logo color inverts in dark mode using CSS classes.
    * Added simple transition for the color switch.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-whitesmoke dark:bg-blacksmoke">
+  <div class="bg-whitesmoke dark:bg-blacksmoke transition-bg ease-in-out duration-1000">
+    <header>
+        <PageNavbar />
+    </header>
     <div class="flex flex-col min-h-screen dark:text-white">
       <slot />
     </div>
