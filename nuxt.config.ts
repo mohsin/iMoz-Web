f48295f57de4de0ace23772048742df638a0da01
Saifur Rahman Mohsin <mohsin92@me.com>
2022-10-22 17:21:42 +0530

    feat: Head title tag added.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+      head: {
+        charset: 'utf-8',
+        viewport: 'width=device-width, initial-scale=1',
+        title: 'iMoz',
+        meta: [
+          { name: 'description', content: 'Saifur Rahman Mohsin\'s Portfolio' }
+        ]
+      },
+    },
     css: ["@/assets/css/styles.css"],
     nitro: {
         preset: 'netlify-edge'
