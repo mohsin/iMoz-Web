a33faf13847308bedb124f3b7d1585ac34e2cf6d
Saifur Rahman Mohsin <mohsin92@me.com>
2022-10-27 11:20:33 +0530

    style: Improved navbar responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="bg-whitesmoke dark:bg-blacksmoke transition-bg ease-in-out duration-1000">
+  <div class="bg-whitesmoke dark:bg-blacksmoke transition-bg ease-in-out duration-1000 flex flex-col min-h-[98vh] w-full">
     <header>
         <PageNavbar />
     </header>
-    <div class="flex flex-col min-h-screen dark:text-white">
-      <slot />
+    <div class="dark:text-white flex grow shrink-0 basis-auto">
+      <div class="pl-4 pr-4">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
