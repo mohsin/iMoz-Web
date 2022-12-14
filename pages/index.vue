50cdd75f0e1607b57c0616f179eb14e8448fe0b0
Saifur Rahman Mohsin <mohsin92@me.com>
2022-12-14 11:54:01 +0530

    feat: Animate the hello wave hand too
    Made the animate work for the wave hand with a change in it's color tone besides the animation on the gravatar.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <article class="m-2 lg:m-6 p-2 lg:p-8">
-        <Gravatar :class="{ 'animate-fadein' : shouldAnimateGravatar }" class="h-32 w-32 rounded-full" :email="email" /><br>
+        <Gravatar :class="{ 'animate-fadein' : shouldAnimate }" class="h-32 w-32 rounded-full" :email="email" /><br>
         <header class="mb-4">
-            <h1 class="text-4xl">Hi 👋</h1>
+            <h1  :class="{ 'animate-fadein' : shouldAnimate }" class="text-4xl">Hi <span class="hidden dark:inline">👋🏼</span><span class="inline dark:hidden">👋🏾</span></h1>
         </header>
         <div>
             <p class="text-md text-slate-500 dark:text-whitesmoke opacity-70">I'm Mohsin, a freelance fullstack engineer based out of Bangalore, India who builds web, mobile, voice and watch apps.</p>
@@ -13,20 +13,20 @@
 <script lang="ts" setup>
 const { isDark } = useMode()
 
-var shouldAnimateGravatar = useState('animate', () => false)
+var shouldAnimate = useState('animate', () => false)
 
 const runtimeConfig = useRuntimeConfig()
 const email = runtimeConfig.emailId;
 
-// Animate gravatar on color mode toggle
-const animateGravatar = () => {
-    if(shouldAnimateGravatar.value) {
-        shouldAnimateGravatar.value = false;
-       window.setTimeout(animateGravatar, 100);
+// Animate element based on color mode toggle
+const animate = () => {
+    if(shouldAnimate.value) {
+        shouldAnimate.value = false
+        window.setTimeout(animate, 100)
     } else {
-        shouldAnimateGravatar.value = true;
-        setTimeout(() => { shouldAnimateGravatar.value = false; console.log('animating complete') }, 2000);
+        shouldAnimate.value = true
+        setTimeout(() => { shouldAnimate.value = false }, 2000)
     }
 }
-watch(isDark, animateGravatar);
+watch(isDark, animate);
 </script>
