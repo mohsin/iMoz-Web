1acbef49726b30cd330e5368ba7321fb2777933f
Saifur Rahman Mohsin <mohsin92@me.com>
2022-12-15 15:47:52 +0530

    refactor: Moved work, education into 1 YAML file.
    The nuxt content plugin was [throwing a warning](https://github.com/nuxt/content/blob/f6ab8bcf92c5a85f46498a0120e51ffed403a815/src/runtime/transformers/yaml.ts#L15) on using arrays as root level elements in YAML. So switched to a single file with 2 keys since it's efficient this way too.

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-var work = (await queryContent('/data/work').findOne()).body
-var education = (await queryContent('/data/education').findOne()).body
+var about = (await queryContent('/data/about').findOne())
 </script>
 
 <template>
@@ -11,7 +10,7 @@ var education = (await queryContent('/data/education').findOne()).body
       <div class="mt-4 pt-0 sm:pt-4 text-justify text-lg text-black dark:text-whitesmoke">
         <div class="flex flex-col justify-center mx-3 sm:mx-0">
           <h2 id="work" class="text-black dark:text-white text-3xl font-extrabold mt-6 w-full text-center sm:text-left">Work</h2>
-          <Accordion :data="work" />
+          <Accordion :data="about.work" />
         </div>
       </div>
     </section>
@@ -19,7 +18,7 @@ var education = (await queryContent('/data/education').findOne()).body
       <div class="mt-4 pt-0 sm:pt-4 text-justify text-lg text-black dark:text-whitesmoke">
         <div class="flex flex-col justify-center mx-3 sm:mx-0">
           <h2 id="work" class="text-black dark:text-white text-3xl font-extrabold mt-6 w-full text-center sm:text-left">Education</h2>
-          <Accordion :data="education" />
+          <Accordion :data="about.education" />
         </div>
       </div>
     </section>
