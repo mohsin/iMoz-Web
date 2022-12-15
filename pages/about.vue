0c5380e814fcbd98ecbec67977cc42e1fce1ed84
Saifur Rahman Mohsin <mohsin92@me.com>
2022-12-15 15:43:15 +0530

    feat: Add education section

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 var work = (await queryContent('/data/work').findOne()).body
+var education = (await queryContent('/data/education').findOne()).body
 </script>
 
 <template>
@@ -11,6 +12,14 @@ var work = (await queryContent('/data/work').findOne()).body
         <div class="flex flex-col justify-center mx-3 sm:mx-0">
           <h2 id="work" class="text-black dark:text-white text-3xl font-extrabold mt-6 w-full text-center sm:text-left">Work</h2>
           <Accordion :data="work" />
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="mt-4 pt-0 sm:pt-4 text-justify text-lg text-black dark:text-whitesmoke">
+        <div class="flex flex-col justify-center mx-3 sm:mx-0">
+          <h2 id="work" class="text-black dark:text-white text-3xl font-extrabold mt-6 w-full text-center sm:text-left">Education</h2>
+          <Accordion :data="education" />
         </div>
       </div>
     </section>
