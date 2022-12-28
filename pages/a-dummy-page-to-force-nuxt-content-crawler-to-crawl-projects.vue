db126d2df7293a84322ab925fdfb21685d1ae5a0
Saifur Rahman Mohsin <mohsin92@me.com>
2022-12-28 20:10:51 +0530

    fix: Workaround to make Nuxt crawl all my projects

---
+++ pages/a-dummy-page-to-force-nuxt-content-crawler-to-crawl-projects.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+var projects = (await queryContent('/data/projects').findOne());
+
+['client', 'opensource', 'others'].forEach((type) => {
+  projects[type].forEach((project) => {
+    try {
+      (queryContent('/data/projects/' + project.slug).findOne())
+    } catch(ex: any) {}
+  })
+})
+</script>
