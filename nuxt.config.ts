7dbf7687257d7446c29e15d53d26133c9621f5a2
Saifur Rahman Mohsin <mohsin92@me.com>
2023-01-04 16:17:23 +0530

    feat: Added favicons to website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,17 @@ export default defineNuxtConfig({
         charset: 'utf-8',
         viewport: 'width=device-width, initial-scale=1',
         title: 'iMoz',
+        link: [
+          { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+          { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+          { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+          { rel: 'manifest', href: '/site.webmanifest' },
+          { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#000000' }
+        ],
         meta: [
-          { name: 'description', content: 'Saifur Rahman Mohsin\'s Portfolio' }
+          { name: 'description', content: 'Saifur Rahman Mohsin\'s Portfolio' },
+          { name: 'msapplication-TileColor', content: '#ffffff' },
+          { name: 'theme-color', content: '#ffffff' }
         ]
       },
     },
@@ -29,5 +38,5 @@ export default defineNuxtConfig({
       public: {
         emailId: ''
       }
-    },
+    }
 })
