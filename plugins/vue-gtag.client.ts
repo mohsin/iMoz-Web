f7b4e43513250e35ed6971ad32f5870954a28d10
Saifur Rahman Mohsin <mohsin92@me.com>
2023-01-07 16:40:42 +0530

    feat(analytics): Check if user uses dark mode

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -1,12 +1,16 @@
 import VueGtag from 'vue-gtag-next'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const { isDark } = useMode()
   const runtimeConfig = useRuntimeConfig()
   const analyticsId = runtimeConfig.gtagAnalyticsId;
 
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: analyticsId
+      id: analyticsId,
+      params: {
+        dark_mode: isDark
+      }
     }
   })
 })
